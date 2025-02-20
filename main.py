07edf53e84d3ea78facbbd53326fe7a40008f233
Fvazquez <fvazquez2@hotmail.com>
2025-02-21 00:33:52 +0100

    casa 21/02/2025

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyttsx3
 import speech_recognition as sr
 import datetime
 
+# claves del registro que indican los idiomas instalados
 id1 = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-ES_HELENA_11.0"
 id2 = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
 
